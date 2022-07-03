121224497e92d53da6b1315e06b695bad4625e18
Hamed Ghoochanian <hamedghoochanian@gmail.com>
2022-07-03 21:11:52 +0430

    added swagger

--- src/main.ts
+++ src/main.ts
@@ -7,12 +7,20 @@ dotenv.config({
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({ transform: true, forbidNonWhitelisted: true }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Store')
+    .setDescription('Store')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,14 +1,11 @@
-import { GoogleRecaptchaFilter } from './google-recaptcha-filter.exception';
 import {
   Body,
   Controller,
   Post,
   SetMetadata,
-  UseFilters,
   UseGuards,
 } from '@nestjs/common';
 import { LoginDto } from './dto/login.dto';
-import { GoogleRecaptchaGuard } from '@nestlab/google-recaptcha';
 import { AuthService } from './auth.service';
 import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { LoginResponse } from './dto/login-response.dto';
@@ -20,8 +17,6 @@ export class AuthController {
 
   @Post('login')
   @SetMetadata('auth', false)
-  @UseGuards(GoogleRecaptchaGuard)
-  @UseFilters(new GoogleRecaptchaFilter())
   @ApiOperation({})
   @ApiResponse({ status: 201, isArray: false, type: LoginResponse })
   async login(@Body() payload: LoginDto): Promise<LoginResponse> {
